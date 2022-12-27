36ae58e5318e267a567051323f17b0a43820f35a
NeAlien <91757246+NeAlien@users.noreply.github.com>
2022-12-27 15:57:05 +0300

    Add files via upload

---
+++ Laba7/Animals/Animal.cs
@@ -0,0 +1,119 @@
+﻿namespace Animals
+{
+    [MyAttribute("Abstract class of animals")]
+    abstract public class Animal
+    {
+        protected eClassificationAnimal Classification;
+        public string Country { get; set; }
+
+        public string Name { get; set; }
+        public string WhatAnimal { get; set; }
+
+        public List<Animal> HideFromOtherAnimals { get; set; }
+        public Animal()
+        {
+            Country = new("");
+            HideFromOtherAnimals = new List<Animal>();
+            Name = new("");
+            WhatAnimal = "Animal";
+        }
+
+        public Animal(string country, List<Animal> hideFromOtherAnimals, string name)
+        {
+            Country = country;
+            HideFromOtherAnimals = hideFromOtherAnimals;
+            Name = name;
+            WhatAnimal = new string("");
+        }
+
+        public void Deconstruct(out string country, out List<Animal> hideFrom, out string name, out string whatAnimal)
+        {
+            country = Country;
+            hideFrom = HideFromOtherAnimals;
+            name = Name;
+            whatAnimal = WhatAnimal;
+        }
+
+        public eClassificationAnimal GetClassificationAnimal() => Classification;
+
+        public abstract eFavouriteFood GetFavouriteFood();
+
+        public abstract void SayHello();
+    }
+
+    [MyAttribute("Animals can be different classifications")]
+    public enum eClassificationAnimal
+    {
+        Herbivores,
+        Carnivores,
+        Omnivores
+    }
+
+    [MyAttribute("Animals have favourite food")]
+    public enum eFavouriteFood
+    {
+        Meat,
+        Plants,
+        Everything
+    }
+
+    [MyAttribute(@"It's a cow")]
+    public class Cow: Animal
+    {
+        public Cow(): base()
+        {
+            WhatAnimal = "Cow";
+            Classification = eClassificationAnimal.Herbivores;
+        }
+        public Cow(string country, List<Animal> hideFromOtherAnimals, string name): 
+            base(country, hideFromOtherAnimals, name)
+        {
+            WhatAnimal = "Cow";
+            Classification = eClassificationAnimal.Herbivores;
+        }
+
+        public override eFavouriteFood GetFavouriteFood() => eFavouriteFood.Plants;
+
+        public override void SayHello() => Console.WriteLine($"Cow says hi{Environment.NewLine}@{Name}");
+    }
+
+    [MyAttribute(@"It's a lion")]
+    public class Lion: Animal
+    {
+        public Lion() : base()
+        {
+            WhatAnimal = "Lion";
+            Classification = eClassificationAnimal.Carnivores;
+        }
+        public Lion(string country, List<Animal> hideFromOtherAnimals, string name):
+            base (country, hideFromOtherAnimals, name)
+        {
+            WhatAnimal = "Lion";
+            Classification = eClassificationAnimal.Carnivores;
+        }
+
+        public override eFavouriteFood GetFavouriteFood() => eFavouriteFood.Meat;
+
+        public override void SayHello() => Console.WriteLine($"Lion says hi{Environment.NewLine}@{Name}");
+    }
+
+    [MyAttribute(@"It's a pig")]
+    public class Pig: Animal
+    {
+        public Pig() : base()
+        {
+            WhatAnimal = "Pig";
+            Classification = eClassificationAnimal.Omnivores;
+        }
+        public Pig(string country, List<Animal> hideFromOtherAnimals, string name):
+            base (country, hideFromOtherAnimals, name)
+        {
+            WhatAnimal = "Pig";
+            Classification = eClassificationAnimal.Omnivores;
+        }
+
+        public override eFavouriteFood GetFavouriteFood() => eFavouriteFood.Everything;
+
+        public override void SayHello() => Console.WriteLine($"Pig says hi{Environment.NewLine}@{Name}");
+    }
+}
---
+++ Laba8/SerializeAnimal/Program.cs
@@ -0,0 +1,52 @@
+﻿using Animals;
+using System.Linq.Expressions;
+using System.Net;
+using System.Xml;
+using System.Xml.Serialization;
+
+Lion Alex = new Lion("USA", new List<Animal>(0), "Alex");
+Pig Marina = new Pig("Germany", new List<Animal> { Alex }, "Marina");
+Cow Masha = new("Russia", new List<Animal> { Alex }, "Masha");
+
+List<Animal> animals = new List<Animal>()
+{
+    Alex,
+    Marina,
+    Masha,
+};
+
+Animal animalToSave = EnterYourChoice(animals);
+
+CreateXmlFile(animalToSave, animalToSave.Name + ".xml");
+
+Console.WriteLine("Data saved!");
+
+void CreateXmlFile(Animal animal, string filename)
+{
+    XmlWriterSettings xmlWriterSettings = new XmlWriterSettings() { Indent = true };
+    XmlSerializer serializer = new XmlSerializer(typeof(Animal), new Type[] { typeof(Cow), typeof(Lion), typeof(Pig) });
+    using (XmlWriter writer = XmlWriter.Create(filename, xmlWriterSettings))
+    {
+        serializer.Serialize(writer, animal);
+    }
+}
+;
+
+Animal EnterYourChoice(List<Animal> animals)
+{
+    Console.WriteLine("Enter, what information you want to save about");
+    int choice = 0;
+    while (choice < 1 || choice > animals.Count)
+    {
+        int i = 1;
+        foreach(Animal animal in animals)
+        {
+            Console.WriteLine($"\t{i++}. {animal.Name}({animal.WhatAnimal})");
+        }
+
+        choice = Convert.ToInt32(Console.ReadLine());
+    }
+
+    return animals[choice - 1];
+}
+
